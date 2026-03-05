11dad47c54cd2b2aa734937af516c9545efbcead
Dinh-Thanh-Phat <thanhphat12tna234@gmail.com>
2026-03-05 16:39:08 +0700

    test: disable excel reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   reporter: [
     ['html', { open: 'never' }],
     ['json', { outputFile: 'test-results/report.json' }],
-    ['./reporters/excel-reporter.ts'],
+    // ['./reporters/excel-reporter.ts'],
   ],
 
   use: {
